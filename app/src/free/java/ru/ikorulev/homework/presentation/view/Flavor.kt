72d6e00eed12774ddce2952589ef869d7455923b
ikorulev <ikorulev@yandex.ru>
2021-08-31 00:11:07 +0300

    Перенес функцию выбора нижнего меню в отдельный класс, который зависит от варианта сборки, также в папки вариантов перенес отображение общего списка Film

---
+++ app/src/free/java/ru/ikorulev/homework/presentation/view/Flavor.kt
@@ -0,0 +1,25 @@
+package ru.ikorulev.homework.presentation.view
+
+import ru.ikorulev.homework.R
+
+class Flavor {
+
+    fun getNavItem(navItem: Int): Int {
+        var nav = 0
+        when (MainActivity.navItem) {
+            R.id.nav_list -> {
+                nav = 1
+            }
+            R.id.nav_watch_later -> {
+                nav = 3
+            }
+            R.id.nav_download -> {
+                nav = 4
+            }
+            R.id.nav_clear -> {
+                nav = 5
+            }
+        }
+        return nav
+    }
+}
